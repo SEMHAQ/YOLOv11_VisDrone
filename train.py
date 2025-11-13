f3672cbb970b17244046e8a8db3708f1fbb89b96
semhaq <2990280345@qq.com>
2025-11-13 23:08:57 +0800

    baseline&LAWDS完成训练和验证，LAWDS没有改进效果。

--- train.py
+++ train.py
@@ -27,14 +27,14 @@ from ultralytics import YOLO
 # YOLO13配置文件路径：ultralytics/cfg/models/13 预训练权重在这里下:https://github.com/iMoonLab/yolov13
 
 if __name__ == '__main__':
-    model = YOLO('ultralytics/cfg/models/11/yolo11-GhostHGNetV2.yaml') # YOLO11
-    # model.load('yolo11n.pt') # loading pretrain weights
+    model = YOLO('ultralytics/cfg/models/11/yolo11-LAWDS.yaml') # YOLO11
+    model.load('yolo11n.pt') # loading pretrain weights
     model.train(data='dataset/data.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=300,
+                epochs=150,
                 batch=32,
-                close_mosaic=0, # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
+                close_mosaic=10, # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
                 workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
                 # device='0,1', # 指定显卡和多卡训练参考<YOLOV11配置文件.md>下方常见错误和解决方案
                 optimizer='SGD', # using SGD
@@ -43,5 +43,5 @@ if __name__ == '__main__':
                 # amp=False, # close amp | loss出现nan可以关闭amp
                 # fraction=0.2,
                 project='runs/train',
-                name='bifpn-p2',
+                name='LAWDS',
                 )
